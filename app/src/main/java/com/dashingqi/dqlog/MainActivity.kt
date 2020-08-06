cb5db1fb801aa6da556e31d07c0511050eff0b07
DashingQi <m13478493231@163.com>
2020-08-06 17:32:04 +0800

    [update]

--- app/src/main/java/com/dashingqi/dqlog/MainActivity.kt
+++ app/src/main/java/com/dashingqi/dqlog/MainActivity.kt
@@ -7,5 +7,18 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        //初始化DQLog
+        DQLog.setDQLogInterceptor(object : DQLogInterceptor {
+            override fun process(level: Int): Boolean {
+                return false
+            }
+
+        })
+
+        DQLog.d("test debug")
+        DQLog.d("debug tag", "test tag debug")
+        DQLog.i("test information")
+        DQLog.w("test warning")
+        DQLog.e("test error")
     }
 }
